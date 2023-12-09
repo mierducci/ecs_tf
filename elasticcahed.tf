ded6316ebb15ae54fcaa9c9338cae9b9318b9d14
georgecaca <georgecaca@gmail.com>
2023-12-09 17:17:20 -0600

    elastic problem

--- elasticcahed.tf
+++ elasticcahed.tf
@@ -12,6 +12,7 @@ resource "aws_elasticache_replication_group" "rep_group" {
   replication_group_id          = "fake-video-studio-rep-group"
   description                 = "example description"
   node_type                     = "cache.t3.micro"
+  num_cache_clusters          =  2
   port                          = 6379
   subnet_group_name             = aws_elasticache_subnet_group.elasticache_subnet.name
 
@@ -25,4 +26,5 @@ resource "aws_elasticache_cluster" "replica" {
   count                = 2
   cluster_id           = "fake-video-studio-rep-group-${count.index}"
   replication_group_id = aws_elasticache_replication_group.rep_group.id
+  
 }
