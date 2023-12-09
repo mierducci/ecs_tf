3461d11e2560b214937b85452aa15f9bbef5d1df
georgecaca <georgecaca@gmail.com>
2023-12-09 17:49:35 -0600

    backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {  
   backend "s3" {  
-    bucket         = "mytfbackend1234567jhgfd"  
+    bucket         = "mytfbackend1234567jhgfd1"  
     key            = "backend.tfstate"  
     region         = "us-east-1"  
     encrypt        = false  
