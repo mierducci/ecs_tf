ee96988a1a28feea34b8b0cb8051ccf73ef7050d
georgecaca <georgecaca@gmail.com>
2023-12-09 19:16:58 -0600

    my error

--- db_subnet_group.tf
+++ db_subnet_group.tf
@@ -1,8 +1,13 @@
-resource "aws_db_subnet_group" "db_subnet_group" {  
-  name       = "db-subnet-group"  
-  subnet_ids = aws_subnet.private_subnet.*.id  
-  tags = {  
-    Name               = "db-subnet-group"  
-    Env                = "production"  
-  }  
+/*
+ * Code to create DB Subnet Group for the private subnets
+ */
+
+resource "aws_db_subnet_group" "db_subnet_group" {
+  name       = "db-subnet-group"
+  subnet_ids = aws_subnet.private_subnet.*.id
+
+  tags = {
+    Name               = "db-subnet-group"
+    Env                = "production"
+  }
 }
